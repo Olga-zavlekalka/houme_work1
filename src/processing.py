45ca6ee16155a693382c6195ab07b37acf32ab87
Olga Zavlekalka <olgusya.zavlekalka@gmail.com>
2025-07-25 21:30:30 +0300

    переименование переменных

--- src/processing.py
+++ src/processing.py
@@ -7,16 +7,16 @@ def filter_by_state(list_dict: List[Dict[str, Any]], state: str = "EXECUTED") ->
     для ключа state(по умолчанию 'EXECUTED'). Функция возвращает новый
     список словарей, содержащий только те словари, у которых ключ state
     cответствует указанному значению."""
-    list_1 = []
+    list_dictionary = []
     for dic in list_dict:
         if dic.get("state") == state:
-            list_1.append(dic)
-    return list_1
+            list_dictionary.append(dic)
+    return list_dictionary
 
 
 def sort_by_date(list_dict: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
     """которая принимает список словарей и необязательный параметр, задающий порядок сортировки
     (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный по дате (date)"""
-    list_2 = sorted(list_dict, key=lambda k: datetime.fromisoformat(k["date"]), reverse=reverse)
+    list_dictionary_2 = sorted(list_dict, key=lambda k: datetime.fromisoformat(k["date"]), reverse=reverse)
 
-    return list_2
+    return list_dictionary_2
