ba72448f001fe5996a856b1917385b994ea3e34d
Olga Zavlekalka <olgusya.zavlekalka@gmail.com>
2025-07-25 19:53:11 +0300

    fixes

--- src/processing.py
+++ src/processing.py
@@ -1,26 +1,22 @@
-#которая принимает список словарей и опционально значение
-# для ключа state(по умолчанию 'EXECUTED'). Функция возвращает новый
-# список словарей, содержащий только те словари, у которых ключ state
-# cответствует указанному значению.
-from typing import List, Dict, Any
 from datetime import datetime
+from typing import Any, Dict, List
 
 
-def filter_by_state(list_dict: List[Dict[str, Any]], state:str ='EXECUTED') ->List[Dict]:
+def filter_by_state(list_dict: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict]:
+    """которая принимает список словарей и опционально значение
+    для ключа state(по умолчанию 'EXECUTED'). Функция возвращает новый
+    список словарей, содержащий только те словари, у которых ключ state
+    cответствует указанному значению."""
     list_1 = []
     for dic in list_dict:
-        if dic.get('state') == state:
+        if dic.get("state") == state:
             list_1.append(dic)
     return list_1
 
 
+def sort_by_date(list_dict: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
+    """которая принимает список словарей и необязательный параметр, задающий порядок сортировки
+    (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный по дате (date)"""
+    list_2 = sorted(list_dict, key=lambda k: datetime.fromisoformat(k["date"]), reverse=reverse)
 
-
-
-
-#которая принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
-# Функция должна возвращать новый список, отсортированный по дате (date).
-def sort_by_date(list_dict: List[Dict[str, Any]], reverse: bool = True) ->List[Dict[str, Any]]:
-    list_2 = sorted(list_dict, key=lambda k:  datetime.fromisoformat(k['date']), reverse=reverse)
     return list_2
-
