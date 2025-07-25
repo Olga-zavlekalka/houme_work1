7bdcccee84e2489cfd32f8b7c23426a417bc5ab6
Olga Zavlekalka <olgusya.zavlekalka@gmail.com>
2025-07-25 19:19:27 +0300

    add function filter_by_state and sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -2,11 +2,25 @@
 # для ключа state(по умолчанию 'EXECUTED'). Функция возвращает новый
 # список словарей, содержащий только те словари, у которых ключ state
 # cответствует указанному значению.
-def filter_by_state():
-    pass
+from typing import List, Dict, Any
+from datetime import datetime
+
+
+def filter_by_state(list_dict: List[Dict[str, Any]], state:str ='EXECUTED') ->List[Dict]:
+    list_1 = []
+    for dic in list_dict:
+        if dic.get('state') == state:
+            list_1.append(dic)
+    return list_1
+
+
+
+
 
 
 #которая принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
 # Функция должна возвращать новый список, отсортированный по дате (date).
-def sort_by_date():
-    pass
+def sort_by_date(list_dict: List[Dict[str, Any]], reverse: bool = True) ->List[Dict[str, Any]]:
+    list_2 = sorted(list_dict, key=lambda k:  datetime.fromisoformat(k['date']), reverse=reverse)
+    return list_2
+
