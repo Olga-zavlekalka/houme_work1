0a0e4f936fc3626dcb9ab4e63799524133ec65cc
Olga Zavlekalka <olgusya.zavlekalka@gmail.com>
2025-07-28 21:17:08 +0300

    add test and chenged widget

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,5 +1,10 @@
-from src.widget import mask_account_card
+from src.widget import mask_account_card, get_date
 import pytest
+
+
+@pytest.mark.parametrize ('str_number, expected', [('Maestro 1596837868705199', 'Maestro 1596 83** **** 5199'), ('Счет 64686473678894779589', 'Счет **9589'), ('MasterCard 7158300734726758', 'MasterCard 7158 30** **** 6758')])
+def test_mask_account_card_with_parametrize(str_number, expected):
+    assert mask_account_card(str_number) == expected
 
 
 def test_mask_account_card():
@@ -7,5 +12,10 @@ def test_mask_account_card():
     assert mask_account_card("Счет 64686473678894779589") == "Счет **9589"
     assert mask_account_card("MasterCard 7158300734726758") == "MasterCard 7158 30** **** 6758"
 
+def test_get_date():
+    assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
 
+def test_get_date_short():
+    with pytest.raises(ValueError):
+        get_date('')
 
