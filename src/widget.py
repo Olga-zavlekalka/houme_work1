7a87ab6d03da0bdf7b3661c7ab276bcd9349ebe2
Olga Zavlekalka <olgusya.zavlekalka@gmail.com>
2025-07-28 21:54:04 +0300

    add tests and chenged processing

--- src/widget.py
+++ src/widget.py
@@ -11,4 +11,6 @@ def mask_account_card(str_number: str) -> str:
 
 def get_date(str_date: str) -> str:
     """функция которая форматирует дату"""
+    if len(str_date) == 0:
+        raise ValueError("Вы не ввели дату")
     return f"{str_date[8:10]}.{str_date[5:7]}.{str_date[:4]}"
