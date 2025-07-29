338db9b1e036821dd89885b203f524565657e0b6
Olga Zavlekalka <olgusya.zavlekalka@gmail.com>
2025-07-29 20:26:58 +0300

    добавлены комментарии (исправлены тесты)

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,32 +1,39 @@
-from src.masks import get_mask_card_number, get_mask_account
 import pytest
 
-
-def test_get_mask_card_number():
-    assert get_mask_card_number('Maestro 1596837868705199') == 'Maestro 1596 83** **** 5199'
-    assert get_mask_card_number('MasterCard 7158300734726758') == 'MasterCard 7158 30** **** 6758'
+from src.masks import get_mask_account, get_mask_card_number
 
 
-def test_get_mask_card_number_shot():
+# функция проверяет работу маскировку карты
+def test_get_mask_card_number() -> None:
+    assert get_mask_card_number("Maestro 1596837868705199") == "Maestro 1596 83** **** 5199"
+    assert get_mask_card_number("MasterCard 7158300734726758") == "MasterCard 7158 30** **** 6758"
+
+
+# функция проверяет которкий ввод данных карты
+def test_get_mask_card_number_shot() -> None:
     with pytest.raises(ValueError):
-       get_mask_card_number('')
+        get_mask_card_number("")
 
 
-def test_get_mask_card_number_long():
+# функция проверяет длинный ввод данных карты
+def test_get_mask_card_number_long() -> None:
     with pytest.raises(ValueError):
-       get_mask_card_number('MasterCard 7158300734726758             ')
+        get_mask_card_number("MasterCard 7158300734726758             ")
 
 
-def test_get_mask_account():
-    assert get_mask_account('Счет 64686473678894779589') == 'Счет **9589'
-    assert get_mask_account("Счет 35383033474447895560") == 'Счет **5560'
+# функция проверяет ввод данных карты
+def test_get_mask_account() -> None:
+    assert get_mask_account("Счет 64686473678894779589") == "Счет **9589"
+    assert get_mask_account("Счет 35383033474447895560") == "Счет **5560"
 
 
-def test_get_mask_account_shot():
+# функция проверяет которкий ввод данных карты
+def test_get_mask_account_shot() -> None:
     with pytest.raises(ValueError):
-       get_mask_account('')
+        get_mask_account("")
 
 
-def test_get_mask_account_long():                            
+# функция проверяет длинный ввод данных карты
+def test_get_mask_account_long() -> None:
     with pytest.raises(ValueError):
-       get_mask_account('Счет 64686473678894779589    ')
+        get_mask_account("Счет 64686473678894779589    ")
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,20 +1,33 @@
-from src.processing import filter_by_state, sort_by_date
 import pytest
 
+from src.processing import filter_by_state, sort_by_date
 
-@pytest.mark.parametrize('state, expected', [('EXECUTED', [{'id': 41428829, 'state': 'EXECUTED',
-                                                            'date': '2019-07-03T18:35:29.512364'},
-                                                           {'id': 939719570, 'state': 'EXECUTED',
-                                                            'date': '2018-06-30T02:08:58.425572'}]),
-                                             ('CANCELED', [{'id': 594226727, 'state': 'CANCELED',
-                                                            'date': '2018-09-12T21:27:25.241689'},
-                                                           {'id': 615064591, 'state': 'CANCELED',
-                                                            'date': '2018-10-14T08:21:33.419441'}])])
 
-def test_filter_by_state_with_parametrize(list_dict, state, expected):
+@pytest.mark.parametrize(
+    "state, expected",
+    [
+        (
+            "EXECUTED",
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        ),
+        (
+            "CANCELED",
+            [
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+        ),
+    ],
+)
+# функция проверяет ожидаемое значение
+def test_filter_by_state_with_parametrize(list_dict: list[dict], state: str, expected: list[dict]) -> None:
     assert filter_by_state(list_dict, state) == expected
 
 
-def test_sort_by_date(list_dict):
+# функция проверяет сортировку по дате
+def test_sort_by_date(list_dict: list[dict]) -> None:
     assert sort_by_date(list_dict, reverse=True)
     assert sort_by_date(list_dict, reverse=False)
